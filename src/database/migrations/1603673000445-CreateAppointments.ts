d878aaa5ad612038658a9502b3093ffc71bc1a77
Rogério Rossa <rogeriorossa@gmail.com>
2020-10-26 21:37:14 -0300

    persistindo appointment no banco

--- src/database/migrations/1603673000445-CreateAppointments.ts
+++ src/database/migrations/1603673000445-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1603673000445
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
