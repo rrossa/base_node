711a225a48d184a88a7f32be11af7e8f4c675fd1
Rogério Rossa <rogeriorossa@gmail.com>
2020-10-28 22:06:53 -0300

    autenticação JWT

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,48 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '@models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorret email/password combination.');
+    }
+
+    // user.password - Senha criptografada
+    // password - Senha não criptografada
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorret email/password combination.');
+    }
+
+    const token = sign({}, '546ad242f8856108443b4a9ae672bc4e', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
